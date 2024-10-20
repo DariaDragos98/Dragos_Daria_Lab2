b8bbe5e6fc5cbd9654e7a384f43177cd172b0b4a
Daria Dragos <Daria Dragos@DARIADRAGOS>
2024-10-20 20:44:31 +0300

    Add project files.

---
+++ Data/Dragos_Daria_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dragos_Daria_Lab2.Models;
+
+namespace Dragos_Daria_Lab2.Data
+{
+    public class Dragos_Daria_Lab2Context : DbContext
+    {
+        public Dragos_Daria_Lab2Context (DbContextOptions<Dragos_Daria_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dragos_Daria_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Dragos_Daria_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Dragos_Daria_Lab2.Models.Authors> Authors { get; set; } = default!;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Dragos_Daria_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+        public int? AuthorsID { get; set; }
+        public Authors? Authors { get; set; }
+
+    }
+}
+
+
+
